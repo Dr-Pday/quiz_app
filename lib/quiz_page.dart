727a9d834542322b273540945a983aae9839cb03
erfan <erfan.blackeye@gmail.com>
2022-09-25 15:12:01 +0330

    listTile widget added

--- lib/quiz_page.dart
+++ lib/quiz_page.dart
@@ -12,7 +12,60 @@ class QuizPage extends StatelessWidget {
         backgroundColor: Colors.indigo[800],
       ),
       body: SafeArea(
-        child: Text(''),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(width: double.infinity),
+              Image.asset(
+                'images/1.png',
+                height: 300,
+              ),
+              SizedBox(height: 25),
+              Text(
+                '۱ سوال',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 30),
+              Text(
+                'بهترین شعبده باز جهان کیست ؟',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 30),
+              ListTile(
+                title: Text(
+                  'پاسخ اول',
+                  textAlign: TextAlign.end,
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  'پاسخ دوم',
+                  textAlign: TextAlign.end,
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  'پاسخ سوم',
+                  textAlign: TextAlign.end,
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  'پاسخ چهارم',
+                  textAlign: TextAlign.end,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
