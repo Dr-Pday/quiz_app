c5f761d954ef481d649549612b368115a0f1ba6c
erfan <erfan.blackeye@gmail.com>
2022-09-30 20:22:33 +0330

    add second question constant

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -10,7 +10,20 @@ List<Question>? getQuestionsList() {
     'علی خامنه ای مزدور'
   ];
   firstQuestion.questionTitle = 'بهترین شعبده باز جهان کیست ؟';
-  firstQuestion.correctanswer = 3;
+  firstQuestion.correctanswer = 2;
   firstQuestion.imageNameNumber = '1';
   firstQuestion.questionNumber = 1;
+
+  var secondQuestion = Question();
+
+  secondQuestion.questionTitle = 'انقلاب سال ۱۴۰۱ با مرگ چه کسی آغاز شد ؟';
+  secondQuestion.answerList = [
+    'مهسا امینی',
+    'حدیثه نجفی',
+    'علی خامنه ای مزدور',
+    'رییسی جاکش'
+  ];
+  secondQuestion.imageNameNumber = '2';
+  secondQuestion.questionNumber = 2;
+  secondQuestion.correctanswer = 0;
 }
